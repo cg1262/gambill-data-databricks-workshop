92826182187ec3d4cfd5a9fde360e6f49ce6408b
chrisgambill30@gmail.com <chrisgambill30@gmail.com>
2026-03-05 15:55:36 +0000

    cg - update clear_resources notebook to be parameterized with widget

--- datatune_bundle/resources/drop_resources.py
+++
@@ -1,6 +0,0 @@
-catalog = "silver_prod"  # Substitute your catalog and environment
-new_schemas = ["sales_ops","sales","marketing"]
- 
-for schema in new_schemas:
-    spark.sql(f"DROP SCHEMA IF EXISTS {catalog}.{schema}")
- 
